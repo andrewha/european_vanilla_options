cd97389b6e17011996f2441c054e068bf6e49223
andrewha <andrey_batyrov@hotmail.com>
2024-12-02 01:11:25 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
     std::cout << "Put  price: $" << std::right << std::setw(5) << default_option.putPrice() << std::endl;
 
     // Create a non-default option
-    std::cout << "\nOption with user-defined parameters: K = $1000.00, r = 2.5%, T = 2 years, S = $1000.00, sigma = 10%" << std::endl;
+    std::cout << "\nOption with user-defined parameters: K = $1,000.00, r = 2.5%, T = 2 years, S = $1,000.00, sigma = 10%" << std::endl;
     const auto non_default_option = EuropeanVanillaOption(1000.0, 0.025, 2.0, 1000.0, 0.1);
     std::cout << "Call price: $" << std::right << std::setw(5) << non_default_option.callPrice() << std::endl;
     std::cout << "Put  price: $" << std::right << std::setw(5) << non_default_option.putPrice() << std::endl;
